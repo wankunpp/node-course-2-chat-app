519f2dccee1f715e3ce3abb70e018f67834bbc01
wankunpp <wankunpp@gmail.com>
2018-06-15 10:32:24 -0400

    add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -5,12 +5,22 @@ socket.on('connect',() =>{
 })
 
 socket.on('newMessage',(message) => {
+    var template = jQuery('#message-template').html();
     var formattedTime = moment(message.createdAt).format('h:mm a');
+    var html = Mustache.render(template,{
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    var li =jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`)
+    jQuery('#messages').append(html);
 
-    jQuery('#messages').append(li);
+    // var formattedTime = moment(message.createdAt).format('h:mm a');
+
+    // var li =jQuery('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`)
+
+    // jQuery('#messages').append(li);
 })
 
 socket.on('disconnect',() =>{
@@ -18,15 +28,24 @@ socket.on('disconnect',() =>{
 })
 
 socket.on('newLocationMessage', (message) =>{
-    var li =jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">current location</a>');
     var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
+    var html =Mustache.render(template,{
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
 
-    li.text(`${message.from} ${formattedTime}: `)
-    a.attr('href', message.url);
-    li.append(a);
+    jQuery('#messages').append(html);
+    // var li =jQuery('<li></li>');
+    // var a = jQuery('<a target="_blank">current location</a>');
+    // var formattedTime = moment(message.createdAt).format('h:mm a');
 
-    jQuery('#messages').append(li);
+    // li.text(`${message.from} ${formattedTime}: `)
+    // a.attr('href', message.url);
+    // li.append(a);
+
+    // jQuery('#messages').append(li);
 })
 
 jQuery('#message-form').on('submit', (e) =>{
--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ io.on('connection',(socket) => {
     socket.broadcast.emit('newMessage', generateMessage('Admin','New User joined'))    
 
     socket.on('createMessage',(message, callback) =>{
-        callback('This is sent from server');
+        callback();
         console.log('createMessage', message);
         io.emit('newMessage',generateMessage(message.from,message.text));
     })
