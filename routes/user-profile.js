e574dc6646dd4930e21a99502b9c47abc79f2224
wankunpp <wankunpp@gmail.com>
2018-08-04 00:43:01 -0400

    fix bug

--- routes/user-profile.js
+++ routes/user-profile.js
@@ -74,7 +74,7 @@ router.post("/me",passport.authenticate('jwt',{
                     firstName: req.body.firstName.trim() , 
                     lastName: req.body.lastName.trim(),
                     // userImage: '/uploads/'+req.file.filename ,
-                    userImage: requ.file.fieldname
+                    userImage: req.file.fieldname
                   }
           }).then(user => {
                 res.redirect('/user-profile/me');
